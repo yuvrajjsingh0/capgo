ec0d379bcc77b4d5503df68df0a412d929e19a62
prakashUXTech <prakashd88@gmail.com>
2022-02-07 10:27:02 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Nav from './components/Nav.vue';
+import Footer from './components/Footer.vue';
+import AppLink from './components/pages/AppLink.vue';
+  
+</script>
+
+<template>
+  <div>
+      <Nav/>
+      <AppLink/>
+      <Footer/>
+  </div>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+    <section class="xl:mx-auto font-fira-sans mx-4 lg:max-w-[85%]">
+        <div class="flex justify-between mt-20 mb-10">
+            <div>
+                <p class="text-[#B2B2B2] text-sm">All rights reserved © CapGo 2021</p>
+            </div>
+            <div>
+                <ul class="text-[#92C8E7] text-sm flex space-x-8">
+                    <li>Contact us</li>
+                    <li>Privacy</li>
+                    <li>Terms</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
